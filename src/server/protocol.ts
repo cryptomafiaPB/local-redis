f9f1d9ba930d6bd5b0c0d518c2ab947a536b8b63
Pranav Sanjay Bagal <jugad360@gmail.com>
2025-10-15 20:32:00 +0530

    Implement Protocol class with RESP parser and encoder integration

---
+++ src/server/protocol.ts
@@ -0,0 +1,23 @@
+import { RESLEncoder, RESPParser, type RESPValue } from "redis-parser-ts"
+
+export class Protocol {
+    private parser: RESPParser;
+    public readonly encoder: typeof RESLEncoder;
+
+    constructor() {
+        this.parser = new RESPParser();
+        this.encoder = RESLEncoder;
+    }
+
+    feed(data: Buffer | string) {
+        this.parser.feed(data);
+    }
+
+    onMessage(callback: (msg: RESPValue) => void) {
+        this.parser.on("message", callback);
+    }
+
+    onError(callback: (err: Error) => void) {
+        this.parser.on("error", callback);
+    }
+}
